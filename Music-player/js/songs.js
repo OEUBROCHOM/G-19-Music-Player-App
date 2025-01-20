02f1095deca3af3f09ee04ca83d7cd5857210b6b
Oeub ROCHOM <oeub.rochom@student.passerellesnumeriques.org>
2025-01-20 18:13:51 +0700

    added songs.js with song code into branch musicplayer and merged to main

---
+++ Music-player/js/songs.js
@@ -0,0 +1,179 @@
+
+
+let allMusic = [
+    {
+      name: "Baby Mama",
+      artist: "Vanda",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Catch me if you can",
+      artist: "Vanda",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Preaneary",
+      artist: "Vanda",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Khmer Blod",
+      artist: "Vanda",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "J + O",
+      artist: "Vanda",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Mnos Laor",
+      artist: "Tena",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Peakly",
+      artist: "Tena",
+      img: "music-7",
+      src: "music-7"
+    },
+  
+    {
+      name: "Propon Komsot",
+      artist: "Tena",
+      img: "music-8",
+      src: "music-8"
+    },
+  
+    {
+      name: "Kong Sakur",
+      artist: "Tena",
+      img: "music-9",
+      src: "music-9"
+    },
+  
+    {
+      name: "Love singular",
+      artist: "Tena",
+      img: "music-10",
+      src: "music-10"
+    },
+    {
+      name: "Songsa Lengleng",
+      artist: "Sokun Kanha",
+      img: "music-11",
+      src: "music-11"
+    },
+  
+    {
+      name: "Samuth Ream",
+      artist: "Sokun Kanha",
+      img: "music-12",
+      src: "music-12"
+    },
+  
+    {
+      name: "Tnoat Te",
+      artist: "Sohun Kanha",
+      img: "music-13",
+      src: "music-13"
+    },
+  
+    {
+      name: "Baby",
+      artist: "Justin",
+      img: "music-14",
+      src: "music-14"
+    },
+  
+    {
+      name: "Should be me",
+      artist: "Justin",
+      img: "music-15",
+      src: "music-15"
+    },
+    {
+      name: "Yumy",
+      artist: "Justin",
+      img: "music-16",
+      src: "music-16"
+    },
+    {
+      name: "Antong sne",
+      artist: "Kemarak Sereymon",
+      img: "music-17",
+      src: "music-17"
+    },
+    {
+      name: "Sabesdong",
+      artist: "Kemarak Sereymon",
+      img: "music-18",
+      src: "music-18"
+    },
+    {
+      name: "Sorry",
+      artist: "Kemarak Sereymon",
+      img: "music-19",
+      src: "music-19"
+    },
+    {
+      name: "Pka Sla",
+      artist: "All3rgy",
+      img: "music-20",
+      src: "music-20"
+    },
+    {
+      name: "Sneha Mech Tove",
+      artist: "Tep Piseth",
+      img: "music-21",
+      src: "music-21"
+    },
+    {
+      name: "Preah Torng",
+      artist: "All3rgy",
+      img: "music-22",
+      src: "music-22"
+    },
+    {
+      name: "Mean Prab Kete",
+      artist: "Preap Sovat",
+      img: "music-23",
+      src: "music-23"
+    },
+    {
+      name: "Sonserm Pkaprey",
+      artist: "Preap Sovat",
+      img: "music-24",
+      src: "music-24"
+    },
+    {
+      name: "Prey Psai",
+      artist: "Preap Sovat",
+      img: "music-25",
+      src: "music-25"
+    },
+    {
+      name: "Alai",
+      artist: "Sinsi Samuth",
+      img: "music-26",
+      src: "music-26"
+    },
+    {
+      name: "Lurch Sne Lurch Tok",
+      artist: "Sinsi Samuth",
+      img: "music-27",
+      src: "music-27"
+    },
+    {
+      name: "Songsa Samlanh Jet",
+      artist: "Sinsi Samuth",
+      img: "music-28",
+      src: "music-28"
+    },
+  ];
